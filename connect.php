7d5a067ff71afe7c1cd75236603ffadc4b1db7e0
Matthew <mmartel2@umbc.edu>
2023-12-07 20:43:13 -0500

    php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
 #connecting to the database
-$conn = mysqli_connect("localhost", "matthew", "2Trwbx81!", "shift_database");
+$conn = mysqli_connect("localhost", "root", "", "shift_database");
 ?>
