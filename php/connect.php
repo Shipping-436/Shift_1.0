fbe4be66ed325552eef89d613e23ab059fbed528
Matthew <mmartel2@umbc.edu>
2023-12-07 20:35:42 -0500

    php work

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,4 @@
 <?php
 #connecting to the database
-$conn = mysqli_connect("localhost", "matthew", "2Trwbx81!", "shift_database") 
-or die(mysqli_error()."Cannot connect to the database.");
+$conn = mysqli_connect("localhost", "matthew", "2Trwbx81!", "shift_database");
 ?>
